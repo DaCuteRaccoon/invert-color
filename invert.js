383ab38d19c358c54fbd726179c0dc08158ce180
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-11-06 12:21:13 -0700

    Create invert.js

---
+++ invert.js
@@ -0,0 +1,17 @@
+// Just type this into your console and it will invert the whole page!
+
+var styles = `
+html {
+    background-color: black;
+}
+
+body {
+    filter: invert(100%);
+    background-color: white;
+    color: black;
+}
+`
+var styleSheet = document.createElement("style")
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
